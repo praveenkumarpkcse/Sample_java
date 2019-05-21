2e28a7b1ab7a9d27676bdc68a5e9e76bb8700962
praveenkumarpkcse <50762992+praveenkumarpkcse@users.noreply.github.com>
2019-05-21 17:17:07 +0530

    Create dot.java

---
+++ dot.java
@@ -0,0 +1,24 @@
+import java.io.*;
+import java.util.*;
+public class Main
+{
+	public static void main(String[] args) {
+		String word;
+		char s1='.';
+		int count=0;
+		Scanner s=new Scanner(System.in);
+		word=s.nextLine();
+		char a[]=word.toCharArray();
+		
+		for(int i=0;i<a.length;i++)
+		{
+		  if(a[i]==s1)
+		  {
+		      count++;
+		  }
+        }
+        System.out.print(count+1);
+	}
+
+	
+}
