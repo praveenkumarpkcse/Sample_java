d251db8789a6e24aea3f413a0922cdf0e4bc2bc1
praveenkumarpkcse <50762992+praveenkumarpkcse@users.noreply.github.com>
2019-05-21 08:52:14 +0530

    Create fib.c

---
+++ fib.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main()
+{
+int fab,f1=1,f2=1,result;
+scanf("%d",&fab);
+for(int i=1;i<fab;i++)
+{
+    printf("%d ",f1);
+    result=f1+f2;
+    f1=f2;
+    f2=result;
+    
+}
+}
